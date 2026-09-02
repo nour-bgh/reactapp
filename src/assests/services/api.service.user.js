a6ce8022d20c863df342a12194d6f90c976f30d6
nourboughzou <nourelhouda.boughzou@fsb.ucar.tn>
2026-09-02 16:17:08 +0100

    add7

--- src/assests/services/api.service.user.js
+++ src/assests/services/api.service.user.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/user";
+const API_URL = `${import.meta.env.VITE_API_URL}/users`;
 
-
-export async function getUserById() {
-    return await axios.get('${API_URL}/getAllusers');
+export async function getAllUsers() {
+    return await axios.get(API_URL);
 }
 
+export async function getUserById(userId) {
+    return await axios.get(`${API_URL}/${userId}`);
+}
 
-export async function deleteuser(userId) {
-    return await axios.delete(`${API_URL}deleteuser/${userId}`);
+export async function createUser(userData) {
+    return await axios.post(API_URL, userData);
+}
+
+export async function updateUser(userId, userData) {
+    return await axios.put(`${API_URL}/${userId}`, userData);
+}
+
+export async function deleteUser(userId) {
+    return await axios.delete(`${API_URL}/${userId}`);
 }
